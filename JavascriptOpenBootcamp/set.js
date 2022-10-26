4a66bc3109643d82982d58a13ec6d3f38e24f1f6
solsona1008 <44673565+solsona1008@users.noreply.github.com>
2022-10-26 19:51:17 +0200

    Create set.js

---
+++ JavascriptOpenBootcamp/set.js
@@ -0,0 +1,12 @@
+const familia = ["Ernesto", "Bea", "Antonio", "Marta"]
+
+const miSet = new Set(familia);
+
+console.log(familia)
+console.log(miSet);
+
+miSet.add("Ernesto")
+console.log(miSet)
+
+miSet.add("Javascript")
+console.log(miSet)
