90259c7c3abbc4e822c77799ec222822cb5fd937
solsona1008 <44673565+solsona1008@users.noreply.github.com>
2022-10-26 18:11:15 +0200

    Create alturaypeso.js

---
+++ JavascriptOpenBootcamp/alturaypeso.js
@@ -0,0 +1,6 @@
+var altura = parseInt(185);
+var alturaMetros = parseFloat(1.85);
+var Peso = parseFloat(83.5);
+var alturaRedondeo = Math.ceil(1.85);
+var Peso = Math.floor(83.5);
+var MaximoValor = Number.MAX_VALUE;
