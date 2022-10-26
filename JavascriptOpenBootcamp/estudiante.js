fa4ffeb06ed570305d7de9e62ab580ac061d232a
solsona1008 <44673565+solsona1008@users.noreply.github.com>
2022-10-26 17:35:22 +0200

    Create estudiante.js

---
+++ JavascriptOpenBootcamp/estudiante.js
@@ -0,0 +1,10 @@
+var nombre = "Adrian";
+var apellido = "Solsona";
+var estudiante = nombre + " " + apellido;
+var estudianteMayus = estudiante.toUpperCase;
+var estudianteMinus = estudiante.toLowerCase;
+console.log(estudiante.length)
+console.log(estudiante.trim().length)
+console.log(estudiante.charAt(0))
+console.log(estudiante.charAt(13))
+console.log(estudiante.includes("Adrian"));
