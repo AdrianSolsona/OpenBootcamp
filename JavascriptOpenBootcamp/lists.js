d4fda7879ab465e709fa39179aee99a7a7b4ef61
solsona1008 <44673565+solsona1008@users.noreply.github.com>
2022-10-26 14:32:19 +0200

    Create lists.js

---
+++ JavascriptOpenBootcamp/lists.js
@@ -0,0 +1,9 @@
+const lista = ["Adrian", 27, "true", "06/05/1995", "book"]
+
+const book = {
+    titulo: "El patron bitcoin",
+    autor: "Saifedean ammous",
+    fecha: new Date("Feb 2 2018"),
+    url: "https://www.amazon.es/patr%C3%B3n-Bitcoin-alternativa-descentralizada-centrales/dp/8423429717"
+}
+console.log(book);
